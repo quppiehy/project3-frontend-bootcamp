6972e3714f0df879baa7d89185549944313cc94e
quppiehy <sunhuiyi@gmail.com>
2023-08-21 11:21:40 +0800

    fixed design and layout

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -2,10 +2,8 @@ import {
   Box,
   Button,
   Typography,
-  Link,
   Divider,
   Paper,
-  IconButton,
   InputBase,
   Radio,
   RadioGroup,
@@ -13,6 +11,7 @@ import {
   FormControl,
   FormLabel,
   Grid,
+  ThemeProvider,
 } from "@mui/material";
 import { Directions } from "@mui/icons-material";
 import React, { useEffect, useState } from "react";
@@ -22,6 +21,7 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 import { clearCart } from "../utils/CartFunctions";
+import { theme2 } from "../theme";
 
 function Payment() {
   const [overallPrice, setOverallPrice] = useState(0);
@@ -78,75 +78,95 @@ function Payment() {
 
   return (
     <>
-      <Typography variant="h3" sx={{ mt: 10 }}>
-        Current Cart
-      </Typography>
-      {overallPrice ? (
-        <Typography variant="h4">Total Cart Price: ${overallPrice}</Typography>
-      ) : (
-        <Typography variant="h4">Total Cart Price:</Typography>
-      )}
+      <Box sx={{ paddingTop: "40px" }}>
+        <ThemeProvider theme={theme2}>
+          <Typography variant="h3" sx={{ mt: 10 }}>
+            Current Cart
+          </Typography>
+          <Box margin="20px">
+            {overallPrice ? (
+              <Typography variant="h4">
+                Total Cart Price: ${overallPrice}
+              </Typography>
+            ) : (
+              <Typography variant="h4">Total Cart Price:</Typography>
+            )}
 
-      <br />
-      <Divider variant="middle" />
-      <br />
+            <br />
+            <Divider variant="middle" />
+            <br />
 
-      <Paper
-        component="form"
-        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
-      >
-        <Typography variant="h6">Coupon Code:</Typography>
-        <InputBase
-          sx={{ marginLeft: 1, flex: 1 }}
-          placeholder="Coupon Code Here"
-        />
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <Directions />
-      </Paper>
-      <br />
-      <Divider variant="middle" />
-      <br />
-      <Typography variant="h4">Payment Options:</Typography>
-      <FormControl>
-        <FormLabel>Select an Option</FormLabel>
-        <RadioGroup
-          value={selectedPaymentOption}
-          onChange={changePaymentOption}
-        >
-          <FormControlLabel
-            value="COD"
-            control={<Radio />}
-            label="Cash on Delivery"
-          />
-          <FormControlLabel
-            value="CDC"
-            control={<Radio />}
-            label="Credit/Debit Card"
-          />
-        </RadioGroup>
-      </FormControl>
-      <br />
-      {selectedPaymentOption === "CDC" ? (
-        <Button
-          variant="contained"
-          size="large"
-          onClick={() => setOpenPaymentBar(true)}
-        >
-          Checkout
-        </Button>
-      ) : (
-        <Button variant="outlined" size="large" onClick={codCheckout}>
-          Cash-On-Delivery Checkout
-        </Button>
-      )}
-      <Divider sx={{ mt: 3, mb: 3 }} />
-      <Grid container justifyContent="center" flexDirection="row">
-        <Grid item sx={{ width: "50%" }}>
-          {openPaymentBar ? (
-            <StripeContainer userId={currUser.id} cost={overallPrice} />
-          ) : null}
-        </Grid>
-      </Grid>
+            <Paper
+              component="form"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+                width: 400,
+              }}
+            >
+              <Typography variant="h6">Coupon Code:</Typography>
+              <InputBase
+                sx={{ marginLeft: 1, flex: 1 }}
+                placeholder="Coupon Code Here"
+              />
+              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+              <Directions />
+            </Paper>
+          </Box>
+        </ThemeProvider>
+
+        <br />
+        <Divider variant="middle" />
+        <br />
+        <ThemeProvider theme={theme2}>
+          <Box margin="20px">
+            <Typography variant="h4">Payment Options:</Typography>
+            <FormControl>
+              <FormLabel>Select an Option</FormLabel>
+              <RadioGroup
+                value={selectedPaymentOption}
+                onChange={changePaymentOption}
+              >
+                <FormControlLabel
+                  value="COD"
+                  control={<Radio />}
+                  label="Cash on Delivery"
+                />
+                <FormControlLabel
+                  value="CDC"
+                  control={<Radio />}
+                  label="Credit/Debit Card"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Box>
+        </ThemeProvider>
+        <br />
+        <Box margin="20px">
+          {selectedPaymentOption === "CDC" ? (
+            <Button
+              variant="contained"
+              size="large"
+              onClick={() => setOpenPaymentBar(true)}
+            >
+              Checkout
+            </Button>
+          ) : (
+            <Button variant="contained" size="large" onClick={codCheckout}>
+              Cash-On-Delivery Checkout
+            </Button>
+          )}
+          <Divider sx={{ mt: 3, mb: 3 }} />
+          <Grid container justifyContent="center" flexDirection="row">
+            <Grid item sx={{ width: "50%" }}>
+              {openPaymentBar ? (
+                <StripeContainer userId={currUser.id} cost={overallPrice} />
+              ) : null}
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
     </>
   );
 }
